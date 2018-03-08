532815d5200fdc63f7f92a635380f7c23ec14aac
Mohit Sharma <msvbs98@gmail.com>
2018-03-08 20:42:22 +0530

    Basic Structure with views and express server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
