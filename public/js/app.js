041e3542af15f6b4c6cf8d9b737090ffc3dd3640
born2bdev <vk530873@gmail.com>
2018-03-09 19:37:39 +0530

    Added other sectional elements

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var exploreImage = false;
+
+var dummyEl = document.getElementById('text');
 setInterval(function () {
 	if(!exploreImage) {
 		exploreImage = true;
@@ -14,3 +16,43 @@ setInterval(function () {
 		$('#explore-image').attr('style','zoomOut');
 	}
 },800);
+
+$(function() {
+  $.scrollify({
+		section:".panel",
+    scrollbars:false,
+    before:function(i,panels) {
+
+      var ref = panels[i].attr("data-section-name");
+
+      $(".pagination .active").removeClass("active");
+
+      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+    },
+    afterRender:function() {
+      var pagination = "<ul class=\"pagination\">";
+      var activeClass = "";
+      $(".panel").each(function(i) {
+        activeClass = "";
+        if(i===0) {
+          activeClass = "active";
+        }
+        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+      });
+
+      pagination += "</ul>";
+
+      $(".home").append(pagination);
+      /*
+
+      Tip: The two click events below are the same:
+
+      $(".pagination a").on("click",function() {
+        $.scrollify.move($(this).attr("href"));
+      });
+
+      */
+      $(".pagination a").on("click",$.scrollify.move);
+    }
+  });
+});
